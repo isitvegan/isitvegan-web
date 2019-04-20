5b56bf4d52a68d78083a8187cd34341072554fba
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-04-20 18:17:50 +0200

    Add lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,14 @@
+#![warn(missing_docs, clippy::dbg_macro, clippy::unimplemented)]
+#![deny(
+    rust_2018_idioms,
+    future_incompatible,
+    missing_debug_implementations,
+    clippy::doc_markdown,
+    clippy::default_trait_access,
+    clippy::enum_glob_use,
+    clippy::needless_borrow,
+    clippy::large_digit_groups,
+    clippy::explicit_into_iter_loop
+)]
+
 pub mod data;
