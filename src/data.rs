6ad3972d6bf72a40e14130948b4f2237c8b4ae0b
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-04-20 18:35:12 +0200

    Add documentation

--- src/data.rs
+++ src/data.rs
@@ -1,24 +1,38 @@
 use serde::{Deserialize, Serialize};
 use url::Url;
 
-#[derive(Serialize, Deserialize)]
+/// The description of an item.
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Item {
+    /// The item's name or title.
     pub name: String,
+
+    /// The item's vegan-ness.
     pub state: State,
+
+    /// A text describing what the item and how it's typically used.
     pub description: String,
+
+    /// List of sources for the item's state.
     #[serde(default)]
     pub sources: Vec<Source>,
 }
 
-#[derive(Serialize, Deserialize)]
+/// The vegan-ness of an item.
+#[derive(Debug, Serialize, Deserialize)]
 pub enum State {
+    /// The item is definitely or overwhelmingly typically vegan.
     Yes,
+    /// The item is probably not vegan
     No,
+    /// The item could be vegan, depending on how it was produced
     Possibly,
 }
 
-#[derive(Serialize, Deserialize)]
+/// A source for an item's [`State`]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Source {
+    /// An online source
     #[serde(with = "url_serde")]
     Url(Url),
 }
