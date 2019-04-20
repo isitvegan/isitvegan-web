39ed62725f502977a170a3ce60af8ebd10777b4b
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-04-20 23:00:02 +0200

    Add create and delete functions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+use is_it_vegan::model::Item;
+use is_it_vegan::model::State;
 use is_it_vegan::search_engine::{ElasticSearch, SearchEngine};
 use is_it_vegan::server::{RocketServer, Server};
 use std::sync::Arc;
 
 fn main() {
     let search_engine = ElasticSearch::try_new(ELASTICSEARCH_ADDRESS, ELASTICSEARCH_PORT).unwrap();
-    for item in search_engine.search("Milk").unwrap() {
-        println!("{:#?}", item);
-    }
+
+    let item = Item {
+        name: "Butter".to_string(),
+        alternative_names: vec!["Titty juice".to_string()],
+        e_numbers: vec![],
+        state: State::Carnist,
+        description: "Not your mom, not your milk!".to_string(),
+        sources: vec![],
+        vegan_alternatives: vec!["Soy Milk".to_string(), "Oat Milk".to_string()],
+    };
+
+    //search_engine.add_item(&item).unwrap();
 
     let server = Box::new(RocketServer::new(Arc::new(search_engine)));
     server.run(SERVER_ADDRESS, PORT).unwrap();
--- src/search_engine.rs
+++ src/search_engine.rs
@@ -8,6 +8,12 @@ use std::fmt::{self, Debug};
 
 /// A search engine for our items
 pub trait SearchEngine: Debug + Sync + Send {
+    /// Add an item to the search engine
+    fn add_item(&self, items: &Item) -> Result<(), Box<dyn Error>>;
+
+    /// Remove all items from the internal storage
+    fn wipe_storage(&self) -> Result<(), Box<dyn Error>>;
+
     /// Search for a query
     fn search(&self, query: &str) -> Result<Vec<Item>, Box<dyn Error>>;
 }
@@ -24,7 +30,7 @@ impl Debug for ElasticSearch {
 }
 
 impl ElasticSearch {
-    /// Create a new [`ElasticSearch`] instant
+    /// Create a new [`ElasticSearch`] instance
     pub fn try_new(address: &str, port: u16) -> Result<ElasticSearch, elastic::Error> {
         let url = format!("{}:{}", address, port);
         Ok(Self {
@@ -34,11 +40,15 @@ impl ElasticSearch {
 }
 
 impl SearchEngine for ElasticSearch {
+    fn add_item(&self, item: &Item) -> Result<(), Box<dyn Error>> {
+        unimplemented!()
+    }
+
     fn search(&self, query: &str) -> Result<Vec<Item>, Box<dyn Error>> {
         Ok(self
             .client
             .search::<Item>()
-            .index("items")
+            .index(INDEX)
             .body(json!({
                 "query": {
                     "query_string": {
@@ -53,3 +63,5 @@ impl SearchEngine for ElasticSearch {
             .collect())
     }
 }
+
+const INDEX: &str = "items";
