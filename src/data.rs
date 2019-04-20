8e589abb3f71d97b0eb8cabb5cfc37b6858da47f
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-04-20 18:21:58 +0200

    Add serde

--- src/data.rs
+++ src/data.rs
@@ -1,18 +1,24 @@
+use serde::{Deserialize, Serialize};
 use url::Url;
 
-pub struct Result {
-    pub item: String,
+#[derive(Serialize, Deserialize)]
+pub struct Item {
+    pub name: String,
     pub state: State,
     pub description: String,
+    #[serde(default)]
     pub sources: Vec<Source>,
 }
 
+#[derive(Serialize, Deserialize)]
 pub enum State {
     Yes,
     No,
     Possibly,
 }
 
+#[derive(Serialize, Deserialize)]
 pub enum Source {
+    #[serde(with = "url_serde")]
     Url(Url),
 }
