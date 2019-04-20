7dc58de93c5568934493acdb80eb0cfdf3e3da87
Jan Nils Ferner <jn_ferner@hotmail.de>
2019-04-20 22:35:24 +0200

    Add some useful data to model

--- src/model.rs
+++ src/model.rs
@@ -9,6 +9,14 @@ pub struct Item {
     /// The item's name or title.
     pub name: String,
 
+    /// Alternative valid names for this item.
+    pub alternative_names: Vec<String>,
+
+    /// [E numbers] assigned to this item.
+    ///
+    /// [E numbers](https://en.wikipedia.org/wiki/E_number)
+    pub e_numbers: Vec<u16>,
+
     /// The item's vegan-ness.
     pub state: State,
 
@@ -18,6 +26,9 @@ pub struct Item {
     /// List of sources for the item's state.
     #[serde(default)]
     pub sources: Vec<Source>,
+
+    /// Common vegan alternatives to this item.
+    pub vegan_alternatives: Vec<String>,
 }
 
 /// The vegan-ness of an item.
