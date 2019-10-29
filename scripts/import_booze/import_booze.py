d1c93f142c71bf36126f59230ecc46a2e313974c
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-10-29 21:00:44 +0100

    Use barnivore api instead of crawling the html site

--- scripts/import_booze/import_booze.py
+++ scripts/import_booze/import_booze.py
@@ -1,39 +1,82 @@
-from bs4 import BeautifulSoup
 import urllib.request
-from itertools import count, chain
+from typing import List
+import json
+import base64
+import os
 from collections import namedtuple
+from itertools import chain
+import sys
+import multiprocessing
 import re
-from concurrent.futures import ThreadPoolExecutor, Future
 from datetime import datetime
+import toml
 
-
-def _get_soup(url):
-    content = urllib.request.urlopen(url)
-    return BeautifulSoup(content, 'html.parser')
-
-
-_BARNIVORE_URL = 'http://www.barnivore.com'
-
-
-def _get_product_soup(product, group, page):
-    product_url = f'{_BARNIVORE_URL}/{product}/{group}?page={page}'
-    return _get_soup(product_url)
-
+_BEER_URL = 'http://barnivore.com/beer.json'
+_WINE_URL = 'http://barnivore.com/wine.json'
+_LIQOR_URL = 'http://barnivore.com/liquor.json'
+_name_re = re.compile(r'[\(\)\-\t]')
 
 _ParsedProduct = namedtuple("_ParsedProduct", "status name url")
 
+def _company_url(company_id: str) -> str:
+    return f'http://barnivore.com/company/{company_id}.json'
 
-def _get_product_from_row(row_soup):
-    status = row_soup.find(class_='status').get_text().strip()
-    product_soup = row_soup.find(class_='info').find(class_='name')
-    name = product_soup.get_text().strip()
-    url = product_soup.a['href'].strip()
+def _fetch_json(url: str):
+    content = urllib.request.urlopen(url).read().decode()
+    return json.loads(content)
 
-    return _ParsedProduct(status, name, url)
+def _fetch_json_cached(url: str):
+    url_hash = base64.b64encode(url.encode('utf-8')).decode('utf-8')
+    cache_file = os.path.join('.cache', f'request-{url_hash}.json')
+    
+    try:
+        if os.path.exists(cache_file):
+            return _read_json(cache_file)
+        else:
+            response = _fetch_json(url)
+            _write_json(cache_file, response)
+            return response
+    except json.decoder.JSONDecodeError as error:
+        raise Exception(f"error trying to decode response for {url} cache_file={cache_file}") from error
 
+def _read_json(file_name: str):
+    with open(file_name) as f:
+        return json.load(f)
 
-_name_re = re.compile(r'[\(\)\-\t]')
+def _write_json(file_name: str, data):
+    with open(file_name, 'w+') as f:
+        json.dump(data, f)
 
+def _fetch_company_ids(url: str) -> List[str]:
+    companies = _fetch_json_cached(url)
+    return [company['company']['id'] for company in companies]
+
+def _fetch_company_products(company_id: str):
+    response = _fetch_json_cached(_company_url(company_id))
+    company = response['company']
+    products = company['products']
+    company_without_products = {key:company[key] for key in company if key != 'products'}
+    return [{**product, 'company': company_without_products} for product in products]
+
+def _fetch_companies() -> List[str]:
+    beer = _fetch_company_ids(_BEER_URL)
+    wine = _fetch_company_ids(_WINE_URL)
+    liqor = _fetch_company_ids(_LIQOR_URL)
+    return [*beer, *wine, *liqor]
+
+def _fetch_products():
+    companies = _fetch_companies()
+    print(f'Fetching products for {len(companies)} companies...', file=sys.stderr)
+    pool = multiprocessing.Pool()
+    products_per_companies = pool.map(_fetch_company_products, companies, 5)
+    return chain(*products_per_companies)
+
+def _map_product(product) -> _ParsedProduct:
+    name = product['product_name']
+    state = product['status']
+    product_id = product['id']
+    url = f'http://www.barnivore.com/products/{product_id}'
+    return _ParsedProduct(state, name, url)
 
 def _map_name(name):
     ugly_name_result = _name_re.search(name)
@@ -60,93 +103,27 @@ def _map_description(status):
 Some variants of this item may contain animal products. 
 Check the provided sources for further details.
 '''
-    return None
-
-
-def _map_url(url):
-    return _BARNIVORE_URL + url
+    return ''
 
 
 def _map_product_to_item(parsed_product):
-    name = _map_name(parsed_product.name)
+    name = parsed_product.name
     state = _map_status_to_state(parsed_product.status)
     description = _map_description(state)
-    url = _map_url(parsed_product.url)
+    url = parsed_product.url
     last_checked = datetime.utcnow().strftime('%Y-%m-%d')
-    item = f'''
-[[items]]
-name = "{name}"
-alternative_names = []
-state = "{state}"
-description = '''
-    if description is None:
-        item += '""'
-    else:
-        item += f'"""{description}"""'
-    item += f'''
-sources = [
-    {{ type = "url", value = "{url}", last_checked = "{last_checked}" }}
-]
-vegan_alternatives = []
-'''
-    return item
-
-
-_GROUPS = [
-    "0-9",
-    "a-f",
-    "g-l",
-    "m-r",
-    "s-t",
-    "u-z"
-]
-
-
-def _import_all_products(product):
-    items = []
-    for group in _GROUPS:
-        for page in count(1):
-            soup = _get_product_soup(product, group, page)
-            products_soup = soup.find('ul', class_='products')
-            if products_soup is None:
-                break
-
-            for index, product_row in enumerate(products_soup.find_all('li')):
-                parsed_product = _get_product_from_row(product_row)
-                item = _map_product_to_item(parsed_product)
-                items.append(item)
-                print(
-                    f'Created {group} {product} item #{page}.{index}: {parsed_product.name}')
-    return items
-
-
-_FILENAME = 'booze.toml'
-
+    return {
+        'name': name,
+        'alternative_names': [],
+        'state': state,
+        'description': description,
+        'sources': [
+            { 'type': 'url', 'value': url, 'last_checked': last_checked },
+        ],
+        'vegan_alternatives': [],
+    }
 
 if __name__ == '__main__':
-    items = []
-    worker_count = 2
-    with ThreadPoolExecutor(max_workers=worker_count) as executor:
-        futures = []
-
-        print(f'Running worker #0 with the import of wine.')
-        future = executor.submit(_import_all_products, 'wine')
-        futures.append(future)
-
-        print(f'Running worker #1 with the import of liquor.')
-        future = executor.submit(_import_all_products, 'liquor')
-        futures.append(future)
-
-        print(f'Running main thread with the import of beer.')
-        items = _import_all_products('beer')
-
-        for future in futures:
-            items.append(future.result())
-
-    items = chain(*items)
-    print(f'Finished creating items, writing them to {_FILENAME}')
-    with open(_FILENAME, 'w') as import_file:
-        for item in items:
-            import_file.write(item)
-
-    print('All done!')
+    products = [_map_product(product) for product in _fetch_products()]
+    items = [_map_product_to_item(product) for product in products]
+    print(toml.dumps({ 'items': items }))
